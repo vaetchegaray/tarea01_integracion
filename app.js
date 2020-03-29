bfe2c986bc65931b78e1ecbce3535a99ecf916cc
Vicente Etchegaray <vaetchegaray@uc.cl>
2020-03-29 00:02:58 -0300

    folder structure & navbar

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const axios = require('axios');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var pjRouter = require('./routes/pj');
+var episodioRouter = require('./routes/episodio');
+var lugarRouter = require('./routes/lugar');
 
 var app = express();
 
@@ -15,20 +18,24 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/episodio', episodioRouter);
+app.use('/pj', pjRouter);
+app.use('/lugar', lugarRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -37,5 +44,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.use(axios);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,32 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'API Rick and Morty'
+  });
 });
+
+/* GET episodios. */
+router.get('/episodio', function (req, res, next) {
+  res.render('episodio', {
+    title: 'Episodios'
+  });
+});
+
+/* GET lugar. */
+router.get('/lugar', function (req, res, next) {
+  res.render('lugar', {
+    title: 'Lugares'
+  });
+});
+
+/* GET personajes. */
+router.get('/pj', function (req, res, next) {
+  res.render('pj', {
+    title: 'Personajes'
+  });
+});
+
 
 module.exports = router;
